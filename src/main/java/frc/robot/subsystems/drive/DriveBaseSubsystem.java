bc3f892377d752134e9285976770356e62bb56fd
Sepandar <sepandar24@gmail.com>
2023-11-29 14:55:32 -0800

    added some path planner stuff
    not sure what is going to happen

--- src/main/java/frc/robot/subsystems/drive/DriveBaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveBaseSubsystem.java
@@ -5,6 +5,12 @@
 package frc.robot.subsystems.drive;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -13,6 +19,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.SwerveConstants;
@@ -156,6 +163,36 @@ public class DriveBaseSubsystem extends SubsystemBase {
   public void setModuleStates(ChassisSpeeds chassisSpeeds) {
     setModuleStates(Constants.SwerveConstants.m_SwerveDriveKinematics.toSwerveModuleStates(chassisSpeeds));
   }
+
+  //TODO: make it return actual stuff
+  public ChassisSpeeds getRobotRelativeSpeeds() {
+    return new ChassisSpeeds();
+  }
+
+  // Assuming this is a method in your drive subsystem
+public Command followPathCommand(String pathName){
+  PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
+  // You must wrap the path following command in a FollowPathWithEvents command in order for event markers to work
+  return new FollowPathWithEvents(
+      new FollowPathHolonomic(
+          path,
+          this::getPose, // Robot pose supplier
+          this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+          this::setModuleStates, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+          new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+              new PIDConstants(2.0, 0.0, 0.0), // Translation PID constants
+              new PIDConstants(2.0, 0.0, 0.0), // Rotation PID constants
+              4.5, // Max module speed, in m/s
+              0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+              new ReplanningConfig() // Default path replanning config. See the API for the options here
+          ),
+          this // Reference to this subsystem to set requirements
+      ),
+      path, // FollowPathWithEvents also requires the path
+      this::getPose // FollowPathWithEvents also requires the robot pose supplier
+  );
+}
+
 
   @Override
   public void periodic() {
