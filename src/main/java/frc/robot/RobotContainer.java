013bf5f6dc4f223745859c90170dd412eb282b0a
Team7419 <126924979+Team7419@users.noreply.github.com>
2023-12-04 16:31:37 -0800

    added choreo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -9,6 +13,7 @@ import frc.robot.subsystems.drive.DriveBaseSubsystem;
 
 public class RobotContainer {
   
+  ChoreoTrajectory trajectory = Choreo.getTrajectory("NewPath");
   // Joysticks, subsystems, and commands must all be private and final
 
   // Joysticks
@@ -44,6 +49,13 @@ public class RobotContainer {
    * @return Auton command
    */
   public Command getAutonomousCommand() {
+
+    // arbitrary for now
+    // PIDController angleController = new PIDController(0.05, 0, 0);
+    // angleController.enableContinuousInput(-Math.PI, Math.PI);
+
+    
+
     return driveBase.followPathCommand("testPath");
     // return new TranslateDistance(driveBase, 1, 0);
   }
