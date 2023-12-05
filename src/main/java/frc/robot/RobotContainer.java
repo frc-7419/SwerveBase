258f3bab7c143de15b2fcc6e17e2096d5ab7e69f
Team7419 <126924979+Team7419@users.noreply.github.com>
2023-12-05 10:17:35 -0800

    pp auton
    Co-Authored-By: Keshav Badrinath <45376658+Aqtion@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -44,7 +46,9 @@ public class RobotContainer {
    * @return Auton command
    */
   public Command getAutonomousCommand() {
-    return driveBase.followPathCommand("testPath");
+
+    return new PathPlannerAuto("Test Auton");
+    // return driveBase.followPathCommand("testPath");
     // return new TranslateDistance(driveBase, 1, 0);
   }
   /**
